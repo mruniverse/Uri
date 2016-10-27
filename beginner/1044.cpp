4bd11a1a90d77a7bc2f59b46e64a4f0d61486485
mruniverse <yurisannin@hotmail.com>
2016-10-27 19:04:45 -0200

    Solved Problem

--- beginner/1044.cpp
+++ beginner/1044.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main(){
-  
+  int a,b;
+
+  cin >> a >> b;
+
+  if(a%2 == 0 && b%2 == 0){
+    printf("Sao Multiplos\n");
+  }
+  else if(a%3 == 0 && b%3 == 0){
+    printf("Sao Multiplos\n");
+  }
+  else if(a%5 == 0 && b%5 == 0){
+    printf("Sao Multiplos\n");
+  }
+  else{
+    printf("Nao sao Multiplos\n");
+  }
+
+  return 0;
 }
